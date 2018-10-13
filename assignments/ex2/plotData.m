cca43f93924363cb94fe10857f81159eab512e1d
dincau33 <dincau33@gmail.com>
2018-10-13 15:42:03 -0400

    Complete ex2 visualize the date exercise

--- assignments/ex2/plotData.m
+++ assignments/ex2/plotData.m
@@ -1,28 +1,16 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
 figure; hold on;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
-
+% Find indices of Admitted and NotAdmitted
+admitted = find(y == 1);
+not_admitted = find(y == 0);
+% Plot training dataset
+plot(X(admitted, 1), X(admitted, 2), 'k+', 'MarkerSize', 3, 'LineWidth', 2);
+plot(X(not_admitted, 1), X(not_admitted, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 5);
 
 hold off;
 
