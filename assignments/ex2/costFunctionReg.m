c879aa57e2caf7983ef3871de10328944095d74a
dincau33 <dincau33@gmail.com>
2018-10-14 16:45:36 -0400

    Complete ex2 cost function and gradient exercise

--- assignments/ex2/costFunctionReg.m
+++ assignments/ex2/costFunctionReg.m
@@ -5,11 +5,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 n = length(theta); % number of features + 1
 [cost, grad] = costFunction(theta, X, y);
+theta_reg = [0;theta(2:n)];
 
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters.
-J = cost + (lambda / (2 * m)) * (theta(2:n))' * theta(2:n);
-grad = grad + (lambda / m) * [0;theta(2:n)];
+J = cost + (lambda / (2 * m)) * (theta_reg)' * theta_reg;
+grad = grad + (lambda / m) * theta_reg;
 
 end
