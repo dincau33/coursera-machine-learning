4b5aba3b1d1d1e200993c40168d2c14929dde61d
dincau33 <dincau33@gmail.com>
2018-10-14 15:27:18 -0400

    Complete ex2 evaluating logistic regression exercise

--- assignments/ex2/predict.m
+++ assignments/ex2/predict.m
@@ -9,8 +9,7 @@ H = sigmoid(X * theta);
 
 %   p = PREDICT(theta, X) computes the predictions for X using a
 %   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
-p(find(H >= 0.5)) = 1;
-p(find(H < 0.5)) = 0;
-p = (p)';
+p = zeros(m, 1);
+p = H >= 0.5;
 
 end
