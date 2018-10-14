1fe0548b1e41c89ef27492a27a2ddf9d045f6e09
dincau33 <dincau33@gmail.com>
2018-10-14 15:21:39 -0400

    Complete ex2 evaluating logistic regression exercise

--- assignments/ex2/predict.m
+++ assignments/ex2/predict.m
@@ -1,20 +1,16 @@
 function p = predict(theta, X)
 %PREDICT Predict whether the label is 0 or 1 using learned logistic
 %regression parameters theta
-%   p = PREDICT(theta, X) computes the predictions for X using a
-%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
 
 m = size(X, 1); % Number of training examples
 
 % Compute probability of p=1 for x given theta
 H = sigmoid(X * theta);
 
-for i=1:m
-  if H(i) >= 0.5
-    p(i, 1) =  1;
-  else
-    p(i, 1) = 0;
-  end
-end
+%   p = PREDICT(theta, X) computes the predictions for X using a
+%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+p(find(H >= 0.5)) = 1;
+p(find(H < 0.5)) = 0;
+p = (p)';
 
 end
