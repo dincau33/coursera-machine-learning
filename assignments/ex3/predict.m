1796303c621db3fd0eab0b43309068362ea83db1
dincau33 <dincau33@gmail.com>
2018-10-24 16:34:14 -0400

    Complete ex3 Feedforward Propagation and Prediction

--- assignments/ex3/predict.m
+++ assignments/ex3/predict.m
@@ -7,29 +7,23 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
+% Calculate value of the hidden layer
+a2 = (sigmoid(Theta1 * (X)'))';
 
+% Add ones to the X data matrix
+a2 = [ones(m, 1) a2];
 
+% Calculate value of the output layer
+a3 = (sigmoid(Theta2 * (a2)'))';
 
-
-
-
-
-
-% =========================================================================
-
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+[~, p] = max(a3, [], 2);
 
 end
