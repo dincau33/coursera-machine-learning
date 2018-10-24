432b032a2631bd249a7d5d3676c50a5f1debbb08
dincau33 <dincau33@gmail.com>
2018-10-24 16:43:16 -0400

    Complete ex3 Feedforward Propagation and Prediction

--- assignments/ex3/predict.m
+++ assignments/ex3/predict.m
@@ -7,20 +7,17 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly
-p = zeros(size(X, 1), 1);
-
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
 % Calculate value of the hidden layer
-a2 = (sigmoid(Theta1 * (X)'))';
+a2 = sigmoid(X * (Theta1)');
 
 % Add ones to the X data matrix
-a2 = [ones(m, 1) a2];
+a2 = [ones(size(a2,1), 1) a2];
 
 % Calculate value of the output layer
-a3 = (sigmoid(Theta2 * (a2)'))';
+a3 = sigmoid(a2 * (Theta2)');
 
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
