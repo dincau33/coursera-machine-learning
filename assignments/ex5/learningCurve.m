bf06c9e0d61e38a45731502f16c5af9c02a11a37
dincau33 <dincau33@gmail.com>
2018-11-13 23:12:45 -0500

    Complete ex5 selecting lambda using a cross validation set

--- assignments/ex5/learningCurve.m
+++ assignments/ex5/learningCurve.m
@@ -40,13 +40,8 @@ error_val   = zeros(m, 1);
 %       Do note that you will still need to use lambda when running
 %       the training to obtain the theta parameters.
 %
-% Hint: You can loop over the examples with the following:
-%
+
 for i = 1:m
-%           % Compute train/cross validation errors using training examples
-%           % X(1:i, :) and y(1:i), storing the result in
-%           % error_train(i) and error_val(i)
-%           ....
   theta = zeros(size(X,2), 1);
   theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
   J_train = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
