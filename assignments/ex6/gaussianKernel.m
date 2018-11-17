c216640e4b85e65eb75d899e2a689f7fa6634340
dincau33 <dincau33@gmail.com>
2018-11-17 10:28:15 -0800

    complete ex6 Gaussian Kerner

--- assignments/ex6/gaussianKernel.m
+++ assignments/ex6/gaussianKernel.m
@@ -6,21 +6,8 @@ function sim = gaussianKernel(x1, x2, sigma)
 % Ensure that x1 and x2 are column vectors
 x1 = x1(:); x2 = x2(:);
 
-% You need to return the following variables correctly.
-sim = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the similarity between x1
-%               and x2 computed using a Gaussian kernel with bandwidth
-%               sigma
-%
-%
-
-
-
-
-
-
-% =============================================================
-    
+% Return the similarity between x1
+% and x2 computed using a Gaussian kernel with bandwidth
+% sigma
+sim = exp(-(1 / (2 * sigma ^ 2)) * (x1 - x2)' * (x1 - x2));
 end
