0b157f0d2f6faaada2236e4b431b3909a8717a12
dincau33 <dincau33@gmail.com>
2018-12-02 23:37:51 -0500

    Complete ex7 implementing PCA

--- assignments/ex7/pca.m
+++ assignments/ex7/pca.m
@@ -11,21 +11,10 @@ function [U, S] = pca(X)
 U = zeros(n);
 S = zeros(n);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should first compute the covariance matrix. Then, you
-%               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
-%
-% Note: When computing the covariance matrix, remember to divide by m (the
-%       number of examples).
-%
+% Covariance calculation
+Sigma = (1 / m) * (X)' * X;
 
-
-
-
-
-
-
-% =========================================================================
+% Compute the eigenvectors and eigenvalues of the covariance matrix
+[U, S, V] = svd(Sigma);
 
 end
